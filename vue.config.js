0a42003b1227b4514bce78fa22b82bf246a3bf8e
ArnaldoAllendeM <arnaldo.allendem@gmail.com>
2022-04-21 01:48:33 -0400

    first commit

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+
+//   entry: {
+//     app: './src/main.js'
+//   },
+//   output: {
+//     path: config.build.assetsRoot,
+//     publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
+//     filename: '[name].js'
+//   },
+//   resolve: {
+//     extensions: ['', '.js', '.vue'],
+//     fallback: [path.join(__dirname, '../node_modules')],
+//     alias: {
+//       'vue': 'vue/dist/vue.common.js',
+//       'src': path.resolve(__dirname, '../src'),
+//       'assets': path.resolve(__dirname, '../src/assets'),
+//       'components': path.resolve(__dirname, '../src/components')
+//     }
+// }
+}
